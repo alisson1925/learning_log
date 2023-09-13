66733c82d3a9eff9a96748adbcb392bfd084065c
Alisson Rodrigues <alisson.r1925@gmail.com>
2023-09-12 21:50:01 -0300

    parte 2 do block

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 def index(request):
     """Página principal do Learning_log"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topics = Topic.objects.order_by('data_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
