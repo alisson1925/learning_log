79446153a5ad5da59cd340dab49741e60198b5bc
Alisson Rodrigues <alisson.r1925@gmail.com>
2023-09-13 14:05:29 -0300

    mostrando o id unico

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,7 +8,15 @@ def index(request):
 
 
 def topics(request):
-    """Mostra todos os assuntos"""
+    """Mostra todos os assuntos."""
     topics = Topic.objects.order_by('data_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-data_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
