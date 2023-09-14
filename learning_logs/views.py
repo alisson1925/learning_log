5b9d199838b0de7f079a36088ddc2b71c5ec30fc
Alisson Rodrigues <alisson.r1925@gmail.com>
2023-09-14 10:46:37 -0300

    formulario

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
@@ -20,3 +23,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-data_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Aciciona um nuvo assunto."""
+    if request.method != 'POST':
+        # Nenhum dado submetido, cria um formulário em branco.
+        form = TopicForm()
+    else:
+        # Dados de POST submetidos; processa dados.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+        
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
